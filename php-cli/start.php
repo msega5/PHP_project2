45e6d3c5c4f76953b547dc21213c72240512ccf3
Сергей Ефанов <msega5@mail.ru>
2024-10-17 11:22:24 +0300

    Add php-cli 4 HW task

--- php-cli/start.php
+++ php-cli/start.php
@@ -1,5 +1,16 @@
 <?php
+$a = 5;
+$b = 9;
 
-echo "Привет, GeekBrains!";
+echo "Было а = $a\n";
+echo "Было b = $b\n";
+
+$a = $a + $b;
+$b = $a - $b;
+
+echo "Стало b = $b\n";
+
+$a = $a - $b;
+echo "Стало a = $a\n";
 
 // docker run --rm -v ${pwd}/php-cli/:/cli php:8.2-cli php /cli/start.php
